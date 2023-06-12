999930bf865d5f3bf7d44f646fd9fc8edb3034bf
yolovi <yolandalopevi@gmail.com>
2023-06-12 23:31:46 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,27 @@
+// BUCLES. Ejercicios para repasar bucles
+
+// Imprimir los números del 21 al 34 en la consola.
+
+for(let i = 21; i <= 34; i++){
+    console.log( i)
+  }
+
+// Sumar los números del 1 al 10 y mostrar el resultado en la consola.
+
+let total= 0
+for (let i = 1; i<=10 ;i++){
+    total += i
+  }
+
+  console.log(total)
+
+// Dado un array de números, imprimir en la consola la suma de todos los números.
+
+
+// Imprimir los números del 1 al 10 en la consola, pero solo los pares.
+// Imprimir los números del 1 al 10 en la consola, pero solo los impares.
+// Imprimir los números del 1 al 20 en la consola, pero para los múltiplos de 3 imprimir "Fizz", para los múltiplos de 5 imprimir "Buzz" y para los múltiplos de ambos imprimir "FizzBuzz".
+// Dado un array de números, imprimir en la consola solo los números pares.
+// Dado un array de números, imprimir en la consola solo los números mayores a 5.
+// Dada una lista de nombres, imprimir solo los nombres que empiezan con la letra "A".
+// Dada una lista de números, encontrar el número más grande y el número más pequeño.
